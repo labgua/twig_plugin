cd9dc64a200c54cda7f0f1c3be99f0dbd4f69d5c
sergio <sergio.guastaferro@yahoo.it>
2020-04-23 23:31:58 +0200

    fix plugin css/js integration
    using call func php reflection & get_header and get_footer include in base.html.twig

--- twig_plugin.php
+++ twig_plugin.php
@@ -2,24 +2,23 @@
 /*
 Plugin Name: Twig Plugin
 Description: Include Twig Engine for make simple themes
-Version: 1.0
+Version: 1.3
 Author: Sergio Guastaferro
 Author URI: https://www.linkedin.com/in/sergio-guastaferro-5826a171
 */
 
-#loading Twig template engine
-require_once dirname(__FILE__).'/twig_plugin/twig/lib/Twig/Autoloader.php';
+//loading other dependencies
+require_once dirname(__FILE__).'/twig_plugin/vendor/autoload.php';
 
-#loading model class of the page
+//loading model class of the page
 require_once dirname(__FILE__).'/twig_plugin/PageModel.class.php';
-
 
 $thisfile=basename(__FILE__, ".php");
 
 register_plugin(
 	$thisfile, 
 	'Twig Plugin', 	
-	'1.0', 		
+	'1.3', 		
 	'Sergio Guastaferro',
 	'https://www.linkedin.com/in/sergio-guastaferro-5826a171', 
 	'Include Twig Engine for make simple themes',
@@ -28,27 +27,30 @@ register_plugin(
 );
 
 
-//# functions
-
-// get an initialized instance of twig
+// functions
+// get an initialized twig instance
 function getTwig(){
 	$dbWebsite = GSDATAOTHERPATH . 'website.xml';
 	$dataWebsite = getXML($dbWebsite);
 	$currThemePath = dirname(__FILE__) . '/../theme/' . $dataWebsite->TEMPLATE; 
 
-	Twig_Autoloader::register(true);
-	$loader = new Twig_Loader_Filesystem( $currThemePath . '/views' );
-	$twig = new Twig_Environment($loader);
+    $loader = new \Twig\Loader\FilesystemLoader($currThemePath . '/views');
+	$twig = new \Twig\Environment($loader);
+
+	$getSimpleWrapFunc = new \Twig\TwigFunction("_gs", function ($function_name, array $params = [] ){
+        return call_user_func_array($function_name, $params);
+    }, ['is_variadic' => true]);
+    $twig->addFunction($getSimpleWrapFunc);
 
 	return $twig;
 }
 
 // get the page-model of the current page, this is the 'context' to pass to twig 
 function getPageModel(){
-	return new Page();
+	return new Labgua\Twig_Plugin\Page();
 }
 
-// a shortcut to render a specific template ( defined in 'theme/MYTHEME/view/' ) with the current page-model  
+// shortcut to render a specific template ( defined in 'theme/MYTHEME/view/' ) with the current page-model
 function render( $template_name ){
 	$template = getTwig()->loadTemplate( $template_name );
 	$pageModel = getPageModel();
